fc7c2ac566bc1baea3916b1e783416b074a46a0d
Jonas-16 <Jonas@vaz.in>
2025-09-03 21:24:38 +0530

    Updated HistoryModal.js

--- src/ui/HistoryModal.js
+++ src/ui/HistoryModal.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from '@emotion/styled';
 import Config from '../core/config/Config';
 
